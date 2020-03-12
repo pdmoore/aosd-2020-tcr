fddf38306d96073252038525144f5a94e64d5fe4
Paul Moore <pdmoore@gmail.com>
2020-03-12 14:13:34 -0700

    empty message

--- spiral-matrix/src/main/java/SpiralMatrixBuilder.java
+++ spiral-matrix/src/main/java/SpiralMatrixBuilder.java
@@ -5,8 +5,10 @@ public class SpiralMatrixBuilder {
         int[][] expected = new int[i][i];
         if (i == 1) {
             expected[0][0] = 1;
-                    
         } else if (i == 2) {
+            expected[0][0] = 1;
+            
+
             expected = new int[][]{
                     {1, 2},
                     {4, 3}
