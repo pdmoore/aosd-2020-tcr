dc4462f75e6de67de7ca1f1d46375cee36d50d22
Paul Moore <pdmoore@gmail.com>
2020-03-12 14:35:04 -0700

    empty message

--- spiral-matrix/src/main/java/SpiralMatrixBuilder.java
+++ spiral-matrix/src/main/java/SpiralMatrixBuilder.java
@@ -17,11 +17,11 @@ public class SpiralMatrixBuilder {
             expected[0][1] = 2;
             expected[0][2] = 3;
             expected[1][2] = 4;
+            expected[2][2] = 5;
             expected[1][0] = 8;
             expected[1][1] = 9;
             expected[2][0] = 7;
             expected[2][1] = 6;
-            expected[2][2] = 5;
 
         }
 
